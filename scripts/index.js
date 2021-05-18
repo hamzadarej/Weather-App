d0b8ff59f22cfcf9141d2c55a560b88f87e09066
hamza <darejhamza@gmail.com>
2021-05-18 11:31:14 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,26 @@
-// selector and listener
-
-const searchbox =
-  // push Enter on keyboard function
-
-  function setQuery(event) {};
-
-// get results function
-
-function getResults(query) {}
-
 // Year for Copyright
-const year = new Date().getFullYear();
+const year = new Date();
 const setYear = document.querySelector(".year");
-setYear.innerHTML = year;
+const getFullYear =year.getFullYear()
+setYear.innerHTML = getFullYear;
+// Current date
+const date = document.querySelector(".date");
 
+const getDayName = () => {
+  return year.toLocaleDateString('en-US', { weekday: "long" });
+};
+const getTime = () => {
+  return year.toLocaleDateString('en-US', { day: "numeric" });
+};
+const getMonthName = () => {
+  return year.toLocaleDateString('en-US', { month: "long" });
+};
+
+date.innerHTML=`${getDayName()} ${getTime()} ${getMonthName()} ${getFullYear}`;
+
+
+
+//Change background depend of the Temperature
 function changeBg() {
   let temp = document.querySelector(".tempN").innerHTML;
   let body = document.querySelector("body");
@@ -26,23 +33,15 @@ function changeBg() {
   }
 }
 changeBg();
+// searchbox addEventlistener & keypress
+const searchBox =document.querySelector(".search-box");
+const city =document.querySelector(".city");
 
-const searchBox = document.querySelector(".search-box");
-searchBox.addEventListener("keypress", searchQuery);
-
-function searchQuery(e) {
-  if (e.keyCode == 13) {
-    getResults(searchBox.value);
+searchBox.addEventListener("keypress",pressEnter);
+function pressEnter(event){
+  if(event.keyCode ==13){
+    city.innerHTML=searchBox.value
+    
   }
 }
 
-function getResults() {}
-
-function dateBuilder() {
-  let today = new Date();
-
-  let date =
-    today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
-
-  return date;
-}
