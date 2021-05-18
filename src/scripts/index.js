cd25b10f9a5f7722e75ef6c4f114f433db7d3e0d
hamza <darejhamza@gmail.com>
2021-05-18 09:23:22 +0200

    WeatherApp

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,16 @@
 const year = new Date().getFullYear();
 const setYear = document.querySelector(".year");
 setYear.innerHTML = year;
+
+function changeBg() {
+  let temp = document.querySelector(".tempN").innerHTML;
+  let body = document.querySelector("body");
+
+  if (temp > 15) {
+    body.style.backgroundImage = "url('images/sunny.jpeg')";
+  }
+  if (temp < 15 && temp > 10) {
+    body.style.backgroundImage = "url('images/Cloudy.jpeg')";
+  }
+}
+changeBg();
