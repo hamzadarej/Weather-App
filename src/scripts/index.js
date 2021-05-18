7b8fa122474837e6db8914e6573d7249cca3042d
igalv <vilenskyigal@gmail.com>
2021-05-18 14:01:38 +0200

    stash

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,35 +1,22 @@
-// selector and listener
-
-const searchbox =
-  // push Enter on keyboard function
-
-  function setQuery(event) {};
-
-// get results function
-
-function getResults(query) {}
-
 // Year for Copyright
 const year = new Date();
 const setYear = document.querySelector(".year");
-const getFullYear =year.getFullYear()
+const getFullYear = year.getFullYear();
 setYear.innerHTML = getFullYear;
 // Current date
 const date = document.querySelector(".date");
 
 const getDayName = () => {
-  return year.toLocaleDateString('en-US', { weekday: "long" });
+  return year.toLocaleDateString("en-US", { weekday: "long" });
 };
 const getTime = () => {
-  return year.toLocaleDateString('en-US', { day: "numeric" });
+  return year.toLocaleDateString("en-US", { day: "numeric" });
 };
 const getMonthName = () => {
-  return year.toLocaleDateString('en-US', { month: "long" });
+  return year.toLocaleDateString("en-US", { month: "long" });
 };
 
-date.innerHTML=`${getDayName()} ${getTime()} ${getMonthName()} ${getFullYear}`;
-
-
+date.innerHTML = `${getDayName()} ${getTime()} ${getMonthName()} ${getFullYear}`;
 
 //Change background depend of the Temperature
 function changeBg() {
@@ -46,26 +33,14 @@ function changeBg() {
 changeBg();
 
 // searchbox eventlistenr & keypress
-const searchBox =document.querySelector(".search-box");
-const city =document.querySelector(".city");
-
-searchBox.addEventListener("keypress",pressEnter);
-function pressEnter(event){
-  if(event.keyCode ==13){
-    city.innerHTML=searchBox.value
-    
-  }
-}
-
-
 const searchBox = document.querySelector(".search-box");
-searchBox.addEventListener("keypress", searchQuery);
+const city = document.querySelector(".city");
 
-function searchQuery(e) {
-  if (e.keyCode == 13) {
-    getResults(searchBox.value);
+searchBox.addEventListener("keypress", pressEnter);
+function pressEnter(event) {
+  if (event.keyCode == 13) {
+    city.innerHTML = searchBox.value;
   }
 }
 
 function getResults() {}
-
