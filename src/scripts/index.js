55c8ed09c3404bed3bc7a2689cd93a0d26d5fb55
hamza <darejhamza@gmail.com>
2021-05-20 12:43:06 +0200

    change the function windDir

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -61,7 +61,7 @@ function displayResults(weather) {
   let wind = document.querySelector(".windDeg");
   wind.innerText = weather.wind.deg;
   //wind directions N,W,S,E
-  function getWind() {
+  /*function getWind() {
     var windDir = weather.wind.deg;
     console.log(windDir);
 
@@ -81,7 +81,21 @@ function displayResults(weather) {
       wind.innerText = "E";
     }
   }
-  getWind();
+  getWind();*/
+  //find the direction 
+  const windD = [
+    { direction: "N", start: 310, end: 360 },
+    { direction: "N", start: 1, end: 50 },
+    { direction: "W", start: 230, end: 309 },
+    { direction: "S", start: 130, end: 229 },
+    { direction: "E", start: 49, end: 129 },
+  ];
+  var windDir = weather.wind.deg;
+  const findDirection = windD.find(
+    (dir) => dir.start <= windDir && dir.end >= windDir
+  );
+  console.log(findDirection.direction);
+  wind.innerHTML = findDirection.direction;
 
   let windSpeed = document.querySelector(".windSpeed");
   windSpeed.innerText = `${weather.wind.speed} km/h`;
@@ -161,8 +175,8 @@ function randomCity() {
     city = defaultCities[Math.floor(Math.random() * defaultCities.length)];
   }
   return city;
-};
+}
 
 function defaultCity() {
   getResults(randomCity());
-};
+}
